e14e8248fdcd21215f58b7324c96c84095255edc
vdinovi <vito.dinovi@gmail.com>
2017-10-21 15:51:30 -0700

    ...

--- find_cst.py
+++ find_cst.py
@@ -1,17 +1,39 @@
-import numpy as np
+#import numpy as np
+import pdb as pdb
 
+"""
 def min_in_s(content, partition):
     arr = []
     num = 0
     for num, c in enumerate(content):
         arr.extend([num+1]*content[num])
-    mat = np.array(0)
     num = 0
+    rows = []
     for p in partition:
-        mat.append(arr[num:p])
+        rows.append(arr[num:num+p])
         num += p
-    #return arr
-    return mat
+    return rows
+"""
+
+def min_in_s(content, partition):
+    arr = [0] * sum(partition)
+    for i in range(1, len(content)+1):
+
+
+
+    """
+    num = 0
+    for num, c in enumerate(content):
+        arr.extend([num+1]*content[num])
+    num = 0
+    rows = []
+    for p in partition:
+        rows.append(arr[num:num+p])
+        num += p
+    return rows
+    """
+    return arr
+
 
 
 result = min_in_s([2,1,1], [3, 1])
